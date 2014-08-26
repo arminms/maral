817abb15231d8329cf619b4c58a1397c987942af
arminms <arminms@3bcb2062-cff5-11e1-a040-2fe239ca460b>
2014-08-26 19:43:33 +0000

    - has_mempber.hpp header was added - maral/bootstraps folder was added - maral/bootstraps/default was implemented

--- trunk/src/maral/ioformats/tree.hpp
+++ trunk/src/maral/ioformats/tree.hpp
@@ -24,6 +24,10 @@
 
 #include <type_traits>
 
+#ifndef MARAL_HAS_MEMBER_HPP
+#include <maral/has_member.hpp>
+#endif // MARAL_HAS_MEMBER_HPP
+
 namespace maral {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -62,71 +66,6 @@ private:
     void print_root_pos(std::ostream& out, const Rt* rt, std::false_type) const
     {}
 };
-
-#if BOOST_WORKAROUND(__clang_major__, BOOST_TESTED_AT(3))
-
-#define GENERATE_HAS_MEMBER(member)                                            \
-                                                                               \
-template < class T >                                                           \
-class HasMember_##member                                                       \
-{                                                                              \
-private:                                                                       \
-    template<typename U, U> struct Check;                                      \
-    using Yes = char[2];                                                       \
-    using  No = char[1];                                                       \
-                                                                               \
-    struct Fallback { int member; };                                           \
-    struct Derived : T, Fallback { };                                          \
-                                                                               \
-    template < class U >                                                       \
-    static No& test (Check<int Fallback::*, &U::member> *);                    \
-    template < typename U >                                                    \
-    static Yes& test ( U* );                                                   \
-                                                                               \
-public:                                                                        \
-    BOOST_STATIC_CONSTEXPR bool RESULT =                                       \
-        sizeof(test<Derived>(nullptr)) == sizeof(Yes);                         \
-};                                                                             \
-                                                                               \
-template < class T >                                                           \
-struct has_member_##member                                                     \
-: public std::integral_constant<bool, HasMember_##member<T>::RESULT>           \
-{ };                                                                           \
-
-#else
-
-#define GENERATE_HAS_MEMBER(member)                                            \
-                                                                               \
-template < class T >                                                           \
-class HasMember_##member                                                       \
-{                                                                              \
-private:                                                                       \
-    using Yes = char[2];                                                       \
-    using  No = char[1];                                                       \
-                                                                               \
-    struct Fallback { int member; };                                           \
-    struct Derived : T, Fallback { };                                          \
-                                                                               \
-    template < class U >                                                       \
-    static No& test ( decltype(U::member)* );                                  \
-    template < typename U >                                                    \
-    static Yes& test ( U* );                                                   \
-                                                                               \
-public:                                                                        \
-    BOOST_STATIC_CONSTEXPR bool RESULT =                                       \
-        sizeof(test<Derived>(nullptr)) == sizeof(Yes);                         \
-};                                                                             \
-                                                                               \
-template < class T >                                                           \
-struct has_member_##member                                                     \
-: public std::integral_constant<bool, HasMember_##member<T>::RESULT>           \
-{ };                                                                           \
-
-#endif  //BOOST_WORKAROUND
-
-GENERATE_HAS_MEMBER(name)       // Creates 'has_member_name'
-GENERATE_HAS_MEMBER(ordinal)    // Creates 'has_member_ordinal'
-GENERATE_HAS_MEMBER(position)     // Creates 'has_member_position'
 
 ////////////////////////////////////////////////////////////////////////////////
 // Implementation
